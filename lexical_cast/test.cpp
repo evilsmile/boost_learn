3ce6028d1bdc21cd0fcf11c4af1f7b9b757d3e67
evilsmile <freedominmind@163.com>
2016-06-30 17:28:47 +0800

    add string convert test

--- lexical_cast/test.cpp
+++ lexical_cast/test.cpp
@@ -14,6 +14,15 @@ int main(int argc, char* argv[])
     cout << "a+1=" << a + 1 << endl;
     cout << "d+1=" << d + 1.3f << endl;
 
+    int i = 888;
+    string s_i = boost::lexical_cast<string>(i);
+    cout << "s_i: " << s_i << endl;
+
+    double f = 823.3f;
+    string s_f = boost::lexical_cast<string>(f);
+    cout << "s_f: " << s_f << endl;
+
+
     try {
         int c = lexical_cast<int>("wrong number");
     } catch (bad_lexical_cast & e) {
